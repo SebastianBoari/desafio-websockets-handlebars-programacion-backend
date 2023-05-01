728c4f6161dd3791d7880556e4479e012d8fe38e
Sebastian Boari <luciosebastianboari@gmail.com>
2023-05-01 13:01:54 -0300

    fix: async callbacks to handler views router - socket server only on realTimeProducts. feat: multer

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,29 +1,97 @@
-const { productManager } = require('../classes/ProductManager');
 const { Router } = require('express');
+const uploader = require('../utils'); 
 const router = Router();
 
 
 // Productos estáticos:
-router.get('/', (req, res) => {
-    const allProducts = productManager.getProducts();
-    
-    res.render('home', {
-        style: 'index',
-        script: 'index',
-        title: 'Productos',
-        products: allProducts
-    });
+router.get('/', async (req, res) => {
+    try {
+        const response = await fetch('http://localhost:8080/api/products');
+        
+        if (!response.ok) {
+            throw new Error('Error al obtener productos');
+        };
+        
+        const products = await response.json();
+        
+        res.render('home', {
+            style: 'index',
+            script: '',
+            title: 'Productos',
+            products: products
+        });
+
+    } catch (error) {
+
+      console.error(error);
+
+      res.status(500).send('Error interno del servidor');
+    };
+});
+  
+
+router.get('/realTimeProducts', async (req, res) => {
+    try {
+        const response = await fetch('http://localhost:8080/api/products');
+        
+        if (!response.ok) {
+            throw new Error('Error al obtener productos');
+        };
+        
+        const products = await response.json();
+        
+        res.render('realTimeProducts', {
+            style: 'index',
+            script: 'index',
+            title: 'Productos',
+            products: products
+        });
+
+    } catch (error) {
+
+      console.error(error);
+
+      res.status(500).send('Error interno del servidor');
+    };
 });
 
-router.get('/realTimeProducts', (req, res) => {
-    const allProducts = productManager.getProducts();
+router.post('/realTimeProducts', uploader.single('file'), async (req, res) => {
+    // TODO: Logica para guardar y mostrar el nuevo producto
+    // Formateo de datos:
+    let status;
+    req.body.status ? status = true : status = false;
     
-    res.render('realTimeProducts', {
-        style: 'index',
-        script: 'index',
-        title: 'Productos',
-        products: allProducts
-    });
+    const product = {
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        status: status,
+        thumbnail: req.body.thumbnail,
+        code: req.body.code,
+        stock: req.body.stock
+    };
+
+    try {
+
+        const response = await fetch('http://localhost:8080/api/products', {
+            method: 'POST',
+            body: JSON.stringify(product),
+            headers: { 'Content-Type': 'application/json' },
+        });
+    
+        if (!response.ok) {
+            throw new Error('Error al agregar producto');
+        };
+    
+        res.send('Producto agregado');
+
+    } catch (error) {
+    
+        console.error(error);
+
+        res.status(500).send('Error interno del servidor');
+
+    };
 });
 
 
